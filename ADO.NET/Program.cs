c51b987721083f796b150eac5cec4783ad8ec04a
Rich Neat <151729655+nc-richard-neat@users.noreply.github.com>
2024-09-10 15:30:46 +0100

    Update Program.cs

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -1,6 +1,2 @@
-﻿using ADO.NET;
-
 var designers = Utils.DeserializeFromFile<List<Designer>>("./Resources/Designers.json");
 var games = Utils.DeserializeFromFile<List<Game>>("./Resources/Games.json");
-
-string connectionString = "Server=<server_name>;Database=MyBoardGameCafe;User Id=<user_id>;Password=<password>;Trust Server Certificate=True";
