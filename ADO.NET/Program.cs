a0d42af27de93eff1c604f7f5d6eacea5d655cf7
Pippa A <107578694+pippaamy@users.noreply.github.com>
2024-09-13 09:57:33 +0100

    Update Program.cs

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -1,2 +1,4 @@
+using ADO.NET;
+
 var designers = Utils.DeserializeFromFile<List<Designer>>("./Resources/Designers.json");
 var games = Utils.DeserializeFromFile<List<Game>>("./Resources/Games.json");
