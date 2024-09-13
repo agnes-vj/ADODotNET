6173ffb935b97872a29eeb7578530ed2e5f8c9f6
Rich Neat <151729655+nc-richard-neat@users.noreply.github.com>
2024-09-13 10:21:10 +0100

    Update Utils.cs

--- ADO.NET/Utils.cs
+++ ADO.NET/Utils.cs
@@ -4,7 +4,19 @@ namespace ADO.NET
 {
     internal class Utils
     {
+        public const string MASTER_CONNECTION_STRING = "Server=localhost\\SQLEXPRESS;Database=master;User Id=rich;Password=northcoders;Trust Server Certificate=True";
         public const string CONNECTION_STRING = "Server=<server_name>;Database=MyBoardGameCafe;User Id=<user_id>;Password=<password>;Trust Server Certificate=True";
+        public static void DropAndCreateDatabase()
+        {
+        using (SqlConnection conn = new SqlConnection(MASTER_CONNECTION_STRING))
+        {
+            conn.Open();
+            SqlCommand dropDb = new SqlCommand("DROP DATABASE IF EXISTS MyBoardGameCafe", conn);
+            SqlCommand createDb = new SqlCommand("CREATE DATABASE MyBoardGameCafe", conn);
+            dropDb.ExecuteNonQuery();
+            createDb.ExecuteNonQuery();
+        }
+        }
         public static T DeserializeFromFile<T>(string filePath)
         {
             var json = File.ReadAllText(filePath);
