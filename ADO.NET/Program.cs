cb074d05591c18f050689f688272936c1ca0c04a
Rich Neat <151729655+nc-richard-neat@users.noreply.github.com>
2024-09-13 10:19:58 +0100

    Update Program.cs

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -2,3 +2,5 @@ using ADO.NET;
 
 var designers = Utils.DeserializeFromFile<List<Designer>>("./Resources/Designers.json");
 var games = Utils.DeserializeFromFile<List<Game>>("./Resources/Games.json");
+
+Utils.DropAndCreateDatabase();
