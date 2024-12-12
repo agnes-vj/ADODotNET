643b16bd6655177465840212caf77abc6bf013be
Agnes <133767448+agnes-vj@users.noreply.github.com>
2024-12-12 11:53:50 +0000

    Add Rows to Designers Table

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -19,4 +19,22 @@ using (SqlConnection connection = new SqlConnection(Utils.CONNECTION_STRING))
     createTable = new SqlCommand("CREATE TABLE Games (Id INT IDENTITY(1,1) PRIMARY KEY, GameName VARCHAR(50),Genre VARCHAR(30), PriceInPence INT, DesignerID INT REFERENCES Designers(Id));", connection);
     dropTable.ExecuteNonQuery();
     createTable.ExecuteNonQuery();
+
+    string firstName = "Elizabeth";
+    string lastName = "Hargrave";
+    string checkQuery = "SELECT 1 FROM Designers WHERE FirstName = '" + firstName + "' AND LastName = '" + lastName + "'";
+    Console.WriteLine(checkQuery);
+
+    string insertQuery = "INSERT INTO Designers(FirstName, LastName) " +
+                                            "SELECT @FirstName, @LastName " +
+                                            "WHERE NOT EXISTS (" + checkQuery + ")";
+    Console.WriteLine(insertQuery);
+    SqlCommand insertCommand = new SqlCommand(insertQuery , connection);
+    
+    insertCommand.Parameters.AddWithValue("@FirstName", firstName);
+    insertCommand.Parameters.AddWithValue("@LastName", lastName);
+
+    int rowsAffected = insertCommand.ExecuteNonQuery();
+    Console.WriteLine(rowsAffected + " Rows Affected");
+
 }
