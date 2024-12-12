984528e91fe3a982f95af8bdc9b26e743511affd
sz-99 <zhengsusan3@gmail.com>
2024-12-12 12:33:07 +0000

    add and delete from games table; add transaction

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -9,32 +9,67 @@ Utils.DropAndCreateDatabase();
 using (SqlConnection connection = new SqlConnection(Utils.CONNECTION_STRING))
 {
     connection.Open();
-    SqlCommand dropTable = new SqlCommand("DROP TABLE IF EXISTS Designers;", connection);
-    SqlCommand createTable = new SqlCommand("CREATE TABLE Designers (Id INT IDENTITY(1,1) PRIMARY KEY, FirstName VARCHAR(50), LastName VARCHAR(50));", connection);
+    SqlTransaction transaction = connection.BeginTransaction();
+    try
+    {
+    SqlCommand dropTable = new SqlCommand("DROP TABLE IF EXISTS Designers;", connection, transaction);
+    SqlCommand createTable = new SqlCommand("CREATE TABLE Designers (Id INT IDENTITY(1,1) PRIMARY KEY, FirstName VARCHAR(50), LastName VARCHAR(50));", connection, transaction);
     dropTable.ExecuteNonQuery();
     createTable.ExecuteNonQuery();
    
 
-    dropTable = new SqlCommand("DROP TABLE IF EXISTS Games;", connection);
-    createTable = new SqlCommand("CREATE TABLE Games (Id INT IDENTITY(1,1) PRIMARY KEY, GameName VARCHAR(50),Genre VARCHAR(30), PriceInPence INT, DesignerID INT REFERENCES Designers(Id));", connection);
+    dropTable = new SqlCommand("DROP TABLE IF EXISTS Games;", connection, transaction);
+    createTable = new SqlCommand("CREATE TABLE Games (Id INT IDENTITY(1,1) PRIMARY KEY, GameName VARCHAR(50),Genre VARCHAR(30), PriceInPence INT, DesignerID INT REFERENCES Designers(Id));", connection, transaction);
     dropTable.ExecuteNonQuery();
     createTable.ExecuteNonQuery();
 
     string firstName = "Elizabeth";
     string lastName = "Hargrave";
     string checkQuery = "SELECT 1 FROM Designers WHERE FirstName = '" + firstName + "' AND LastName = '" + lastName + "'";
-    Console.WriteLine(checkQuery);
 
     string insertQuery = "INSERT INTO Designers(FirstName, LastName) " +
                                             "SELECT @FirstName, @LastName " +
                                             "WHERE NOT EXISTS (" + checkQuery + ")";
-    Console.WriteLine(insertQuery);
-    SqlCommand insertCommand = new SqlCommand(insertQuery , connection);
+
+    SqlCommand insertCommand = new SqlCommand(insertQuery , connection, transaction);
     
     insertCommand.Parameters.AddWithValue("@FirstName", firstName);
     insertCommand.Parameters.AddWithValue("@LastName", lastName);
 
     int rowsAffected = insertCommand.ExecuteNonQuery();
-    Console.WriteLine(rowsAffected + " Rows Affected");
+
+    SqlCommand insertGameCmd = new SqlCommand("INSERT INTO Games (GameName, Genre, PriceInPence, DesignerID)" +
+                                                "SELECT 'Wingspan', 'EngineBuilding', 3500, 1" +
+                                                "WHERE NOT EXISTS(" +
+                                                "SELECT 1 " +
+                                                "FROM Games " +
+                                                "WHERE GameName = 'Wingspan');", connection, transaction);
+
+    int rowsAffected2 = insertGameCmd.ExecuteNonQuery();
+
+
+
+    SqlCommand insertGame2Cmd = new SqlCommand("INSERT INTO Games (GameName, Genre, PriceInPence, DesignerID)" +
+                                               "SELECT 'Mariposas', 'Set Collection', 3000, 1" +
+                                               "WHERE NOT EXISTS(" +
+                                               "SELECT 1 " +
+                                               "FROM Games " +
+                                               "WHERE GameName = 'Mariposas');", connection, transaction);
+
+    int rowsAffected3 = insertGame2Cmd.ExecuteNonQuery();
+    Console.WriteLine(rowsAffected3 + "rows affected in Games table");
+
+
+    SqlCommand deleteGame2Cmd = new SqlCommand("DELETE FROM Games WHERE GameName = 'Mariposas'", connection, transaction);
+    int rowsDeleted = deleteGame2Cmd.ExecuteNonQuery();
+        Console.WriteLine(rowsDeleted + "rows deleted");
+
+        transaction.Commit();
+    }
+    catch(Exception ex)
+    {
+        transaction.Rollback();
+        Console.WriteLine(ex.Message);
+    }
 
 }
