e124b3014861df08db297e2a8d02e1bfe06f9f57
Agnes <133767448+agnes-vj@users.noreply.github.com>
2024-12-12 17:07:53 +0000

    Add code to display records from Game and Designers Table joined

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -1,5 +1,7 @@
 using ADO.NET;
 using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
 
 var designers = Utils.DeserializeFromFile<List<Designer>>("./Resources/Designers.json");
 var games = Utils.DeserializeFromFile<List<Game>>("./Resources/Games.json");
@@ -31,37 +33,37 @@ using (SqlConnection connection = new SqlConnection(Utils.CONNECTION_STRING))
                                             "SELECT @FirstName, @LastName " +
                                             "WHERE NOT EXISTS (" + checkQuery + ")";
 
-    SqlCommand insertCommand = new SqlCommand(insertQuery , connection, transaction);
-    
-    insertCommand.Parameters.AddWithValue("@FirstName", firstName);
-    insertCommand.Parameters.AddWithValue("@LastName", lastName);
+        SqlCommand insertCommand = new SqlCommand(insertQuery, connection, transaction);
 
-    int rowsAffected = insertCommand.ExecuteNonQuery();
+        insertCommand.Parameters.AddWithValue("@FirstName", firstName);
+        insertCommand.Parameters.AddWithValue("@LastName", lastName);
 
-    SqlCommand insertGameCmd = new SqlCommand("INSERT INTO Games (GameName, Genre, PriceInPence, DesignerID)" +
-                                                "SELECT 'Wingspan', 'EngineBuilding', 3500, 1" +
-                                                "WHERE NOT EXISTS(" +
-                                                "SELECT 1 " +
-                                                "FROM Games " +
-                                                "WHERE GameName = 'Wingspan');", connection, transaction);
+        int rowsAffected = insertCommand.ExecuteNonQuery();
 
-    int rowsAffected2 = insertGameCmd.ExecuteNonQuery();
+        SqlCommand insertGameCmd = new SqlCommand("INSERT INTO Games (GameName, Genre, PriceInPence, DesignerID)" +
+                                                    "SELECT 'Wingspan', 'EngineBuilding', 3500, 1" +
+                                                    "WHERE NOT EXISTS(" +
+                                                    "SELECT 1 " +
+                                                    "FROM Games " +
+                                                    "WHERE GameName = 'Wingspan');", connection, transaction);
 
-
-
-    SqlCommand insertGame2Cmd = new SqlCommand("INSERT INTO Games (GameName, Genre, PriceInPence, DesignerID)" +
-                                               "SELECT 'Mariposas', 'Set Collection', 3000, 1" +
-                                               "WHERE NOT EXISTS(" +
-                                               "SELECT 1 " +
-                                               "FROM Games " +
-                                               "WHERE GameName = 'Mariposas');", connection, transaction);
-
-    int rowsAffected3 = insertGame2Cmd.ExecuteNonQuery();
-    Console.WriteLine(rowsAffected3 + "rows affected in Games table");
+        int rowsAffected2 = insertGameCmd.ExecuteNonQuery();
 
 
-    SqlCommand deleteGame2Cmd = new SqlCommand("DELETE FROM Games WHERE GameName = 'Mariposas'", connection, transaction);
-    int rowsDeleted = deleteGame2Cmd.ExecuteNonQuery();
+
+        SqlCommand insertGame2Cmd = new SqlCommand("INSERT INTO Games (GameName, Genre, PriceInPence, DesignerID)" +
+                                                   "SELECT 'Mariposas', 'Set Collection', 3000, 1" +
+                                                   "WHERE NOT EXISTS(" +
+                                                   "SELECT 1 " +
+                                                   "FROM Games " +
+                                                   "WHERE GameName = 'Mariposas');", connection, transaction);
+
+        int rowsAffected3 = insertGame2Cmd.ExecuteNonQuery();
+        Console.WriteLine(rowsAffected3 + "rows affected in Games table");
+
+
+        SqlCommand deleteGame2Cmd = new SqlCommand("DELETE FROM Games WHERE GameName = 'Mariposas'", connection, transaction);
+        int rowsDeleted = deleteGame2Cmd.ExecuteNonQuery();
         Console.WriteLine(rowsDeleted + "rows deleted");
 
 
@@ -76,20 +78,110 @@ using (SqlConnection connection = new SqlConnection(Utils.CONNECTION_STRING))
         //        Console.Write(reader.GetValue(2) + "   ");
         //    }
         //}
-        SqlCommand insertDesignerRows; 
-        foreach(Designer designer in designers)
-        {
-            insertDesignerRows = new SqlCommand("INSERT INTO Designers VALUES (@Name1, @Name2)", connection, transaction);
-            insertDesignerRows.Parameters.AddWithValue("@Name1", designer.FirstName);
-            insertDesignerRows.Parameters.AddWithValue("@Name2", designer.LastName);
-            insertDesignerRows.ExecuteNonQuery();
+        InsertDesignerRecords(designers, connection, transaction);
+       InsertGamesRecords(games, connection, transaction);
+        //DisplayDesignerRecords( connection, transaction);
+        //DisplayGameRecords( connection, transaction);
+
+        SqlCommand command = new("SELECT d.FirstName + ' ' + d.LastName as Designer, g.* FROM Games g JOIN Designers d ON g.DesignerId = d.Id", connection, transaction);
+        using (SqlDataReader reader = command.ExecuteReader())
+        {    
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write($"{reader.GetName(i),-20}");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-', reader.FieldCount * 20));
+
+            while (reader.Read())
+            {
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                { 
+                    Console.Write($"{reader.GetValue(i), -20}");
+                }
+                Console.WriteLine();
+
+            }
         }
-        transaction.Commit();
+            transaction.Commit();
     }
     catch(Exception ex)
     {
         transaction.Rollback();
         Console.WriteLine(ex.Message);
+    }
+
+}
+
+static void DisplayGameRecords(SqlConnection connection, SqlTransaction transaction)
+{
+    SqlCommand command = new($"SELECT * FROM Games;", connection, transaction);
+
+
+    using (SqlDataReader reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+
+                Console.Write(reader.GetValue(i) + "   ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
+static void DisplayDesignerRecords(SqlConnection connection, SqlTransaction transaction)
+{
+    SqlCommand command = new($"SELECT * FROM Designers;", connection, transaction);
+
+
+    using (SqlDataReader reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+
+                Console.Write(reader.GetValue(i) + "   ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
+static void InsertDesignerRecords(List<Designer> designers, SqlConnection connection, SqlTransaction transaction)
+{
+    SqlCommand insertDesignerRows;
+    foreach (Designer designer in designers)
+    {
+        insertDesignerRows = new SqlCommand("INSERT INTO Designers " +
+         "SELECT @FirstName, @LastName" +
+         " WHERE NOT EXISTS(SELECT 1 FROM Designers WHERE FirstName = @FirstName AND LastName=@LastName);", connection, transaction);
+        insertDesignerRows.Parameters.AddWithValue("@FirstName", designer.FirstName);
+        insertDesignerRows.Parameters.AddWithValue("@LastName", designer.LastName);
+        insertDesignerRows.ExecuteNonQuery();
+    }
+
+}
+
+static void InsertGamesRecords(List<Game> games, SqlConnection connection, SqlTransaction transaction)
+{
+    SqlCommand insertDesignerRows;
+    foreach (Game game in games)
+    {
+        insertDesignerRows = new SqlCommand("INSERT INTO Games " +
+         "SELECT @GameName, @Genre, @Price, @DesignerId" +
+         " WHERE NOT EXISTS(SELECT 1 FROM Games WHERE GameName = @GameName);", connection, transaction);
+        insertDesignerRows.Parameters.AddWithValue("@GameName", game.GameName);
+        insertDesignerRows.Parameters.AddWithValue("@Genre", game.Genre);
+        insertDesignerRows.Parameters.AddWithValue("@Price", game.Price);
+        insertDesignerRows.Parameters.AddWithValue("@DesignerId", game.DesignerId);
+        insertDesignerRows.ExecuteNonQuery();
     }
 
 }
