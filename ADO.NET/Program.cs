bd3928c3f45514d00ac6d3a3b87d9dfd8c874551
Agnes <133767448+agnes-vj@users.noreply.github.com>
2024-12-12 11:00:17 +0000

    Add Games Table

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -12,6 +12,11 @@ using (SqlConnection connection = new SqlConnection(Utils.CONNECTION_STRING))
     SqlCommand dropTable = new SqlCommand("DROP TABLE IF EXISTS Designers;", connection);
     SqlCommand createTable = new SqlCommand("CREATE TABLE Designers (Id INT IDENTITY(1,1) PRIMARY KEY, FirstName VARCHAR(50), LastName VARCHAR(50));", connection);
     dropTable.ExecuteNonQuery();
-    var result = createTable.ExecuteNonQuery();
-    Console.WriteLine(result + "rows affected");
+    createTable.ExecuteNonQuery();
+   
+
+    dropTable = new SqlCommand("DROP TABLE IF EXISTS Games;", connection);
+    createTable = new SqlCommand("CREATE TABLE Games (Id INT IDENTITY(1,1) PRIMARY KEY, GameName VARCHAR(50),Genre VARCHAR(30), PriceInPence INT, DesignerID INT REFERENCES Designers(Id));", connection);
+    dropTable.ExecuteNonQuery();
+    createTable.ExecuteNonQuery();
 }
